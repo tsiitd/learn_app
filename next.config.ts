fc7e2016c6788bd54567ddff86223e130336e45b
tsiitd <ts.tushar@protonmail.com>
2025-11-30 11:15:54 +0400

    build version made

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/learn_app', // Replace 'learn_app' with your GitHub repository name
+  images: {
+    unoptimized: true, // Required for static export
+  },
 };
 
 export default nextConfig;
